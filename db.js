cf4bbdcd3fe80c44e81590f8eb99be119d3585f7
Hernán Eche <hernan_eche@yahoo.com.ar>
2016-06-14 12:41:11 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,20 @@
 var db=[{"name":"twitter",		"descentralized": false},
 	{"name":"facebook",		"descentralized": false},
 	{"name":"fb",			"descentralized": false},
-	{"name":"email",		"descentralized": true},
-	{"name":"mail",			"descentralized": true},
 	{"name":"npm",			"descentralzied": false},
 	{"name":"whatsapp",		"descentralized": false},
-	{"name":"git",			"descentralized": true},
-	{"name":"dns", 			"descentralized": true},
 	{"name":"david-dm",		"descentralized": false},
 	{"name":"stackoverflow",	"descentralized": false},
-	{"name":"github",		"descentralized": false}];
+	{"name":"github",		"descentralized": false},
+	{"name":"disqus",		"descentralized": false},
+	{"name":"email",		"descentralized": true},
+	{"name":"mail",			"descentralized": true},
+	{"name":"git",			"descentralized": true},
+	{"name":"rss",			"descentralized": true},
+	{"name":"web",			"descentralized": true},
+	{"name":"tcp",			"descentralized": true},
+	{"name":"udp",			"descentralized": true},
+	{"name":"dns", 			"descentralized": true}];
 
 	var exMessages=["will be totally extinct.", "extinct.", "go away.", "good bye.", "time will take you to oblivion, forever.", "is that thing still around?" ];
 	var goMessages=["long life to you!", "you seems to be very welcome in the world!", "you survive!", "perhaps aeons will see you! good luck!", "you will live!"];
